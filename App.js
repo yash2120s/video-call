1e32e711f626f2b1a9a8d20dc5a6d380726a8362
yash2120s <80688710+yash2120s@users.noreply.github.com>
2022-01-25 22:38:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Navigation from './src/navigation';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle={'dark-content'} />
+
+      <Navigation />
+    </>
+  );
+};
+
+export default App;
